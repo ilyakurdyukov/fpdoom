c4f27184bce05de0026641342275c1584fa7170d
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2023-04-20 09:59:11 +0700

    better printf (but also bigger)

--- fpdoom/libc/printf.h
+++ fpdoom/libc/printf.h
@@ -22,11 +22,18 @@ static void fprintf_put(void *param, int ch) {
 	fputc(ch, x->file);
 }
 
+static void printf_putn(void (*put)(void*, int), void *param, int ch, int n) {
+	while (n > 0) { put(param, ch); n--; }
+}
+
 static void printf_body(void (*put)(void*, int), void *param, const char *fmt, va_list va) {
 	int a;
 	for (;;) {
 		const char *fmt1;
-		int width, prec, fill;
+		const char *str;
+		unsigned len, hex_alpha, flags = 0;
+		int width, prec;
+		uintptr_t num;
 
 #define PUT(a) put(param, a)
 
@@ -36,104 +43,137 @@ static void printf_body(void (*put)(void*, int), void *param, const char *fmt, v
 			PUT(a);
 			continue;
 		}
-		width = 0; prec = -1; fill = ' ';
-		
+
+		width = 0; prec = -1;
 		fmt1 = fmt;
+		a = *fmt++;
+		// flags
 		for (;;) {
-			const char *str;
-			unsigned len, hex_alpha, bin_alt;
-			uintptr_t num;
+			if (a == '0') flags |= 1;
+			else if (a == '#') flags |= 2;
+			else if (a == '-') flags |= 4;
+			else if (a == '+') flags |= 8;
+			else if (a == ' ') flags |= 16;
+			else break;
 			a = *fmt++;
-			if (a == 's') {
-				str = va_arg(va, const char*);
-				if (!str) str = "(null)";
-str1:
-				len = 0;
-				while (len < (unsigned)prec && str[len]) len++;
-				while (width > (int)len) { PUT(' '); width--; }
-				while (len) { PUT(*str++); len--; }
-				break;
-
-			} else if (a == 'u' || a == 'i' || a == 'd') {
-				char buf[10]; // 32bit = 10, 64bit = 20
-				int sign = 0;
-				if (width < prec) width = prec;
-				num = va_arg(va, unsigned);
-				if (a != 'u') {
-					sign = (int)num >> 31;
-					num = ((int)num + sign) ^ sign;
-					width += sign;
-				}
-				len = 0;
-				do {
-					a = num + '0'; num /= 10;
-					buf[len++] = a - num * 10;
-				} while (num);
-				while (width > (int)len) {
-					a = width > prec ? fill : '0';
-					PUT(a); width--;
-				}
-				if (sign) PUT('-');
-				while (len) PUT(buf[--len]);
-				break;
-
-			} else if (a == 'c') {
-				while (width > 1) { PUT(' '); width--; }
-				a = va_arg(va, int);
-				PUT(a);
-				break;
-
-			} else if (a == 'x' || a == 'X') {
-				bin_alt = 0;
-				if (width < prec) width = prec;
-				hex_alpha = 'a' - '0' - 10 - 'x' + a;
-				num = va_arg(va, unsigned);
-do_hex:
-				for (len = sizeof(void*) * 2; len > 1; len--) {
-					if (num >> (sizeof(num) * 8 - 4)) break;
-					num <<= 4;
-				}
-				if (bin_alt) len += 2;
-				while (width > (int)len) {
-					a = width > prec ? fill : ' ';
-					PUT(a); width--;
-				}
-				if (bin_alt) { PUT('0'); PUT('x'); len -= 2; }
-				while (len) {
-					a = num >> (sizeof(num) * 8 - 4);
-					num <<= 4;
-					if (a >= 10) a += hex_alpha;
-					PUT(a + '0');
-					len--;
-				}
-				break;
-
-			} else if (a == 'p') {
-				void *ptr = va_arg(va, void*);
-				hex_alpha = 'a' - '0' - 10;
-				if (!ptr) { str = "(nil)"; goto str1; }
-				bin_alt = 1;
-				num = (uintptr_t)ptr;
-				goto do_hex;
-
-			} else if ((unsigned)(a - '0') < 10) {
-				a -= '0';
-				if (prec < 0) {
-					if (!a && width == 0) fill = '0';
-					width = width * 10 + a;
-					if (width >> 16) goto bad;
-				} else {
-					prec = prec * 10 + a;
-					if (prec >> 16) goto bad;
-				}
-			} else if (a == '%') {
-				PUT(a);
-				break;
-			} else if (a == '.') {
-				if (prec >= 0) goto bad;
-				prec = 0;
-			} else goto bad;
 		}
+		// width
+		if (a == '*') {
+			width = va_arg(va, int);
+			if (width >> 16) goto bad;
+			a = *fmt++;
+		} else
+		while ((unsigned)a - '0' < 10) {
+			width = width * 10 + (a - '0');
+			if (width >> 16) goto bad;
+			a = *fmt++;
+		}
+		// prec
+		if (a == '.') {
+			a = *fmt++;
+			prec = 0;
+			if (a == '*') {
+				prec = va_arg(va, int);
+				if (prec >> 16) goto bad;
+				a = *fmt++;
+			} else
+			while ((unsigned)a - '0' < 10) {
+				prec = prec * 10 + (a - '0');
+				if (prec >> 16) goto bad;
+				a = *fmt++;
+			}
+		}
+		// length
+		if (a == 'z') {	// size_t
+			a = *fmt++;
+		} else if (a == 'l') {	// long
+			a = *fmt++;
+		}
+		// type
+		if (a == 's') {
+			str = va_arg(va, const char*);
+			if (!str) str = "(null)";
+str1:
+			len = 0;
+			while (len < (unsigned)prec && str[len]) len++;
+			width -= len;
+			if (!(flags & 4)) printf_putn(put, param, ' ', width);
+			while (len) { PUT(*str++); len--; }
+
+		} else if (a == 'u' || a == 'i' || a == 'd') {
+			char buf[10]; // 32bit = 10, 64bit = 20
+			int sign = 0;
+			if (width < prec) width = prec;
+			num = va_arg(va, unsigned);
+			if (a != 'u') {
+				sign = (int)num >> 31;
+				num = ((int)num + sign) ^ sign;
+				width += sign;
+				sign &= '-';
+				if (!sign && (flags & (8 | 16))) {
+					width -= 1;
+					sign = flags & 8 ? '+' : ' ';
+				}
+			}
+			len = 0;
+			do {
+				a = num + '0'; num /= 10;
+				buf[len++] = a - num * 10;
+			} while (num);
+			if (prec < (int)len) prec = len;
+			if ((flags & 5) == 1 && width > prec) prec = width;
+			width -= prec;
+			if (!(flags & 4)) printf_putn(put, param, ' ', width);
+			if (sign) PUT(sign);
+			printf_putn(put, param, '0', prec - len);
+			while (len) PUT(buf[--len]);
+
+		} else if (a == 'c') {
+			a = va_arg(va, int);
+			width -= 1;
+			if (!(flags & 4)) printf_putn(put, param, ' ', width);
+			PUT(a);
+
+		} else if (a == 'x' || a == 'X') {
+			if (width < prec) width = prec;
+			hex_alpha = 'a' - '0' - 10 - 'x' + a;
+			num = va_arg(va, unsigned);
+do_hex:
+			if (flags & 2) width -= 2;
+			for (len = sizeof(void*) * 2; len > 1; len--) {
+				if (num >> (sizeof(num) * 8 - 4)) break;
+				num <<= 4;
+			}
+			if (prec < (int)len) prec = len;
+			if ((flags & 5) == 1 && width > prec) prec = width;
+			width -= prec;
+			if (!(flags & 4)) printf_putn(put, param, ' ', width);
+			if (flags & 2) {
+				PUT('0'); PUT('x' - 'a' + '0' + 10 + hex_alpha);
+			}
+			printf_putn(put, param, '0', prec - len);
+			while (len) {
+				a = num >> (sizeof(num) * 8 - 4);
+				num <<= 4;
+				if (a >= 10) a += hex_alpha;
+				PUT(a + '0');
+				len--;
+			}
+
+		} else if (a == 'p') {
+			void *ptr = va_arg(va, void*);
+			hex_alpha = 'a' - '0' - 10;
+			if (!ptr) { str = "(nil)"; goto str1; }
+			flags |= 2;
+			num = (uintptr_t)ptr;
+			goto do_hex;
+
+		} else if (a == '%') {
+			PUT(a);
+			continue;
+		} else goto bad;
+
+		if (flags & 4) printf_putn(put, param, ' ', width);
 		continue;
 bad:
 		fmt = fmt1;
